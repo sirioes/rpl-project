136e300e643f41433ab6e8f06a4f5d4394f46302
Nidio <nidio.tilman24@gmail.com>
2026-04-07 01:11:37 +0700

    feat: create user login page UI with language switcher

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+
+class SetLocale
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Session::has('locale')) {
+            App::setLocale(Session::get('locale'));
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,16 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('lang/{locale}', function ($locale) {
+    if (in_array($locale, ['en', 'id', 'nl', 'de', 'fr'])) {
+        session()->put('locale', $locale);
+    }
+    return redirect()->back();
+})->name('lang.switch');
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
