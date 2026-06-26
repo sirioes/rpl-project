5db3d5a36257838bb2c7f4ab040056c3ee7b0718
Risma Putri <wirdianthi05@gmail.com>
2026-06-26 12:34:48 +0800

    feat: implement factory pattern via RepositoryServiceProvider

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\Contracts\BookingRepositoryInterface;
+use App\Repositories\Contracts\ProductRepositoryInterface;
+use App\Repositories\Contracts\TrackRecordRepositoryInterface;
+use App\Repositories\Contracts\UserRepositoryInterface;
+use App\Repositories\Eloquent\EloquentBookingRepository;
+use App\Repositories\Eloquent\EloquentProductRepository;
+use App\Repositories\Eloquent\EloquentTrackRecordRepository;
+use App\Repositories\Eloquent\EloquentUserRepository;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->bind(UserRepositoryInterface::class, EloquentUserRepository::class);
+        $this->app->bind(BookingRepositoryInterface::class, EloquentBookingRepository::class);
+        $this->app->bind(ProductRepositoryInterface::class, EloquentProductRepository::class);
+        $this->app->bind(TrackRecordRepositoryInterface::class, EloquentTrackRecordRepository::class);
+    }
+}
